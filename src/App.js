efff79afc5bb54664fb7d30acba5fbf1599aae2f
Fedor B Gorsky <fedor@support-pc.org>
2022-03-10 18:51:31 +0300

    wip login

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Admin, Resource } from 'react-admin';
 import {TopicsList} from "./topics";
 import {TopicCreate} from "./TopicCreate";
 import {TopicEdit} from "./TopicEdit";
+import AuthProvider from "./authProvider";
+import LoginPage from "./LoginPage";
 
 const dataProvider = jsonServerProvider('http://localhost:9090');
 
 
 const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin dataProvider={dataProvider} authProvider={AuthProvider} loginPage={LoginPage}>
         <Resource name="admin/topics" list={TopicsList} options={{ label: 'Topics' }} create={TopicCreate} edit={TopicEdit}/>
     </Admin>
 );
