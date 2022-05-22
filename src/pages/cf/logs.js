2c4de871c31139232801142e7ddb7702a78668b0
Fedor B Gorsky <fedor@support-pc.org>
2022-05-22 06:41:37 +0300

    breadcrumbs

--- src/pages/cf/logs.js
+++ src/pages/cf/logs.js
@@ -1,17 +1,36 @@
 import * as React from "react";
 import {List, Datagrid, TextField} from 'react-admin';
 import {useParams} from "react-router-dom"
+import {Breadcrumbs} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Link from '@mui/material/Link';
+
 
 export const LogList = props => {
 
     const {id} = useParams();
 
     return (
-        <List {...props} resource={`/admin/cf/${id}/log`}>
-            <Datagrid>
-                <TextField source="run_at"/>
-                <TextField source="result"/>
-            </Datagrid>
-        </List>
+        <div>
+            <Breadcrumbs aria-label="breadcrumb">
+                <Link underline="hover" color="inherit" href='/admin/cf/'>
+                    Functions
+                </Link>
+                <Link
+                    underline="hover"
+                    color="inherit"
+                    href={`/admin/cf/${id}`}
+                >
+                    {id}
+                </Link>
+                <Typography color="text.primary">Logs</Typography>
+            </Breadcrumbs>
+            <List {...props} resource={`/admin/cf/${id}/log`}>
+                <Datagrid>
+                    <TextField source="run_at"/>
+                    <TextField source="result"/>
+                </Datagrid>
+            </List>
+        </div>
     );
 }
