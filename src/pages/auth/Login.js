c4a9a3ee43cecd30146bdb247e20d4d8d53ccd79
Fedor B Gorsky <fedor@support-pc.org>
2022-06-24 06:57:24 +0300

    add oauth providers

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { useState } from 'react';
-import { useLogin, useNotify } from 'react-admin';
+import {useState} from 'react';
+import {useLogin, useNotify} from 'react-admin';
 import {apiUrl, httpClient} from "../../utils/http";
 
-const LoginPage = ({ theme }) => {
+const LoginPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const login = useLogin();
@@ -11,39 +11,49 @@ const LoginPage = ({ theme }) => {
 
     const handleSubmit = e => {
         e.preventDefault();
-        login({ email, password }).catch(() =>
+        login({email, password}).catch(() =>
             notify('Invalid email or password')
         );
     };
 
-    const github = e => {
-        httpClient(`${apiUrl}/api/user/oauth/github/link`, {method: 'GET'})
+    const oauth = (provider) => {
+        httpClient(`${apiUrl}/api/user/oauth/${provider}/link`, {method: 'GET'})
             .then(value => {
 
                 window.location.href = value.body
             })
     }
 
+    const github = () => {
+        oauth('github')
+    }
+    const vk = () => {
+        oauth('vk')
+    }
+    const yandex = () => {
+        oauth('yandex')
+    }
+
     return (
-
         <>
-
-        <form onSubmit={handleSubmit}>
-            <input
-                name="email"
-                type="email"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-            />
-            <input
-                name="password"
-                type="password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-            />
-        </form>
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="email"
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <input
+                    name="password"
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+            </form>
 
             <button onClick={github}>Github</button>
+            <button onClick={vk}>Vk</button>
+            <button onClick={yandex}>Yandex</button>
 
         </>
     );
